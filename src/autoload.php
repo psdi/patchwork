39942beecaedee857dafede6adf840a4aa17ac14
Psalm Diao <p.diao25@gmail.com>
2019-04-18 21:29:09 +0200

    move code

--- src/autoload.php
+++ src/autoload.php
@@ -13,9 +13,3 @@ $loader->addNamespaceGroup('Test', 'test/', function (Library\Autoloader $a) {
 });
 
 $loader->register();
-
-// test
-$logger = new Library\Logger(dirname(__DIR__, 1) . '/test/data/logs');
-$logger->write(__CLASS__, __METHOD__, 'success', 'I did it! Or rather, I implemented a thing that was already given in the interwebs haha');
-
-$testClass = new Test\Foo\Bar\TestClass();

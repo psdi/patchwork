6d11ab39ea0425873251e35ae4c689ef172c9422
Psalm Diao <p.diao25@gmail.com>
2019-04-18 21:11:29 +0200

    update .gitignore, limit usage of directory_separator constant

--- src/autoload.php
+++ src/autoload.php
@@ -11,7 +11,7 @@ $loader->addNamespace('Test\\Foo\\Bar', 'test/foo/bar/');
 $loader->register();
 
 // test
-$logger = new Library\Logger(dirname(__DIR__, 1) . 'test/data/logs');
+$logger = new Library\Logger(dirname(__DIR__, 1) . '/test/data/logs');
 $logger->write(__CLASS__, __METHOD__, 'success', 'I did it! Or rather, I implemented a thing that was already given in the interwebs haha');
 
 $testClass = new Test\Foo\Bar\TestClass();

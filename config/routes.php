5ed24af846f737a94ccc47fbfa62a9d7ca4cb260
Psalm Diao <p.diao25@gmail.com>
2019-06-06 21:18:46 +0200

    revamp dispatch method and start on abstract controller

--- config/routes.php
+++ config/routes.php
@@ -6,14 +6,11 @@ use Http\Request;
 return function (Request $request) {
     $router = new Router($request);
 
-echo '<pre>';
-
     $router->addRoute('/hello/:lang/:num', 'GET', [ 'Controllers\TestController::returnAction' ], [
         'required' => [
             'lang' => '\w+',
             'num' => '\d+',
         ],
-        // 'default' => [ 'lang' => 'en' ],
     ]);
 
     $router->addRoute('/hello', 'GET', [ 'Controllers\TestController::listAction' ]);
